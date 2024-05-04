58fd819ecaff54b5ea0644d470aac020975cad80
mehul <mehulbisen1@gmail.com>
2024-05-04 14:59:28 +0530

    infra-till-ec2

--- three-tier.tf
+++ three-tier.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "project_vpc" {
     cidr_block = "10.0.0.0/16"
 }
 
-# Creating Subnets
+# Creating Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.project_vpc
   cidr_block = "10.0.1.0/24"
@@ -16,6 +16,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+# Creating Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.project_vpc
   cidr_block = "10.0.18.0/24"
@@ -38,7 +39,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.project_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"  # Route all traffic to the internet
+    cidr_block = "0.0.0.0/0"  
     gateway_id = aws_internet_gateway.project_igw.id
   }
 }
@@ -58,4 +59,87 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route_table_association" "private_route_table_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create Elastic IP
+resource "aws_eip" "project_eip" {
+  vpc = true
+}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "project_nat" {
+  allocation_id = aws_eip.project_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+}
+
+# Create Route for NAT Gateway
+resource "aws_route" "nat_gateway_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.project_nat.id
+}
+
+# Create Security Group
+resource "aws_security_group" "project_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Database_instance
+resource "aws_instance" "my_instance" {
+  ami           = "ami-12345678"  
+  instance_type = "t2.micro"     
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = aws_key_pair.my_key_pair.key_name
+  security_groups = [aws_security_group.project_security_group.name]
+
+  tags = {
+    Name = "Database_instance"
+  }
+}
+
+# Application_server_instance
+resource "aws_instance" "aap_server" {
+  ami           = "ami-12345678"  
+  instance_type = "t2.micro"     
+  subnet_id     = aws_subnet.private_subnet.id
+  key_name      = aws_key_pair.my_key_pair.key_name
+  security_groups = [aws_security_group.project_security_group.name]
+
+  tags = {
+    Name = "App_server_instance"
+  }
+}
+
+# Frontend_Server
+resource "aws_instance" "Frontend_Server" {
+  ami           = "ami-12345678"  
+  instance_type = "t2.micro"     
+  subnet_id     = aws_subnet.private_subnet.id
+  key_name      = aws_key_pair.my_key_pair.key_name
+  security_groups = [aws_security_group.project_security_group.name]
+
+  tags = {
+    Name = "frontend_server_instance"
+  }
 }
