20051b3d37416b6fb245297643f0be060c5d6049
mehul <mehulbisen1@gmail.com>
2024-05-06 15:21:34 +0530

    update-9

--- three-tier.tf
+++ three-tier.tf
@@ -111,7 +111,7 @@ resource "aws_db_instance" "myinstance" {
   identifier           = "myrdsinstance"
   allocated_storage    =  20
   engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t3.micro"
   username             = "admin"
   password             = "redhat12345"
   parameter_group_name = "default.mysql5.7"
