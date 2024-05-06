61ffdd235d67a6f959cda4b336cd73863ff4c995
mehul <mehulbisen1@gmail.com>
2024-05-07 01:19:15 +0530

    update-6

--- Terraform-prod.tf
+++ Terraform-prod.tf
@@ -126,29 +126,6 @@ resource "aws_security_group" "project_security_group" {
   }
 }
 
-# Creating RDS Subnet Group
-resource "aws_db_subnet_group" "project_subnet_group" {
-  name       = "project-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet.id, aws_subnet.private_subnet2.id]
-}
-
-# Creating RDS 
-resource "aws_db_instance" "my_rds_instance" {
-  identifier             = "my-rds-instance"
-  allocated_storage      = 20
-  storage_type           = "gp2"
-  engine                 = "mysql" 
-  engine_version         = "5.7"
-  instance_class         = "db.t3.micro" 
-  username               = "admin"
-  password               = "redhat12345"
-  parameter_group_name   = "default.mysql5.7"
-  vpc_security_group_ids = [aws_security_group.project_security_group.id]
-  publicly_accessible    = false 
-  db_subnet_group_name = aws_db_subnet_group.project_subnet_group.name
-  skip_final_snapshot    = true
-}
-
 # Creating Load Balancer
 resource "aws_lb" "Demo-ALB" {
   name               = "Demo-ALB"
