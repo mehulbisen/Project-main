02271cc1c6b4d34cf050d7c80d77e12c05668662
mehul <mehulbisen1@gmail.com>
2024-05-06 15:16:00 +0530

    update-7

--- three-tier.tf
+++ three-tier.tf
@@ -176,3 +176,19 @@ EOF
     Name = "database_server_instance"
   }
 }
+
+# Creating RDS 
+resource "aws_db_instance" "myinstance" {
+  engine               = "mysql"
+  identifier           = "myrdsinstance"
+  allocated_storage    =  20
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = "admin"
+  password             = "redhat12345"
+  parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = [aws_security_group.project_security_group.id]
+  skip_final_snapshot  = true
+  publicly_accessible =  true
+
+}
